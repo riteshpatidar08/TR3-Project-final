40a0fc8f01e7cb39635049e19b96a5469f71c52e
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-10-08 13:09:37 +0530

    updatedCode

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const colors = require('colors')
 const dbConnect = async() => {
   try {
-    const connection = await mongoose.connect('mongodb://localhost:27017/ecommerce');
+    const connection = await mongoose.connect('mongodb://localhost:27017/shopping-data');
     console.log(colors.blue('Connection Successfull'))
   } catch (error) {
     console.log(colors.red(error))
